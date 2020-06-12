d0af5ddf22e9f7c55b060ef53eaaecbb0d2f252a
Saurav Giri <64953856+xaea-12@users.noreply.github.com>
2020-06-12 18:46:15 +0545

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -57,6 +57,7 @@ async function main() {
       });
       console.log(`Found ${data.total_count} runs total.`);
       const runningWorkflows = data.workflow_runs.filter(
+        console.log(workflow.head_sha+" --- "+headSha)
         workflow => workflow.status !== 'completed'
       );
       console.log(`Found ${runningWorkflows.length} runs in progress.`);
